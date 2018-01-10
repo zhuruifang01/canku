883362e9dd586cc034effb7b18c14c19d2dc3acf
Your Name <you@example.com>
2018-01-10 17:35:44 +0800

    canku

--- js/createcharts.js
+++ js/createcharts.js
@@ -1,6 +1,6 @@
   var createcharts={};//创建函数对象
   // 入库管理
-
+      //柱图1
       createcharts.chart1=function(Xdata,Ydata,Ydata2){
       $(function(){
           var chart1= echarts.init(document.getElementById('chart1'));
@@ -130,6 +130,7 @@
       });
 
   };
+      // 横线柱图
       createcharts.chart2=function(title,Xdata,Ydata,Ydata2){
       $(function(){
           var chart2= echarts.init(document.getElementById('chart2'));
@@ -233,6 +234,7 @@
       });
 
   };
+      // 占比柱图1（入库）
       createcharts.chart3=function(zhuX,zhuY1,zhuY2){
       $(function(){
           var chart3= echarts.init(document.getElementById('chart3'));
@@ -291,97 +293,92 @@
 
               ],
               series : [
-                  { // For shadow
-                      label: {
-                          normal: {
-                              show: true,
-                              position: 'top',
-                              textStyle: {
-                                  color: '#96E3D8',
-                                  fontSize :16
-                              },
-                              formatter: '{c}'
-                          }
-                      },
-                      type: 'bar',
-                      name:"应收货量",
-                      barWidth: '50%',
-                      itemStyle:{
-                          normal: {
-                              barBorder:1,
-                              barBorderColor: '#96E3D8',
-                              barBorderRadius: 0,
-                              color: new echarts.graphic.LinearGradient(0, 1, 0, 1, [{
-                                  offset: 0,
-                                  color: '#FFF'
-                              }, {
-                                  offset: 1,
-                                  color: '#FFF'
-                              }]),
-                              shadowColor: 'rgba(0, 0, 0, 0)',
-                              shadowBlur: 20
-                          }
-                      },
-                      barGap:'-100%',
-                      barCategoryGap:'40%',
-                      data:zhuY1,
-                      animation: false
-                  },
                   {
-                      label: {
-                          normal: {
-                              show: true,
-                              position: 'top',
-                              textStyle: {
-                                  color: '#66C5F6',
-                                  fontSize :14
+                      // For shadow
+                              label: {
+                                  normal: {
+                                      show: true,
+                                      position: 'top',
+                                      textStyle: {
+                                          color: '#96E3D8',
+                                          fontSize :16
+                                      },
+                                      formatter: '{c}'
+                                  }
                               },
-                              formatter: '{c}'
-                          }
-                      },
-                      name:'已收货量',
-                      type:'bar',
-                      barWidth: '50%',
-                      itemStyle:{
-                          normal: {
 
-                              barBorderRadius: 0,
-                              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                                  offset: 0,
-                                  color: '#96E3D8'
-                              }, {
-                                  offset: 1,
-                                  color: '#66C5F6'
-                              }]),
-                              shadowColor: 'rgba(0, 0, 0, 0)',
-                              shadowBlur: 20
-                          }
-                      },
-                      data:zhuY2
+                              type: 'bar',
+                              name:"应收货量",
+                              barWidth: '50%',
+                              itemStyle:{
+                                  normal: {
+                                      barBorderWidth:1, //边框宽度
+                                      barBorderColor: '#96E3D8', //边框色
+                                      barBorderRadius: 0,
+                                      color: '#fff', //填充色
+                                  }
+                              },
+                              barGap:'-100%',
+                              barCategoryGap:'40%',
+                              data:zhuY1,
+                              animation: false
+                  },
+
+                  {
+                          label: {
+                              normal: {
+                                  show: true,
+                                      position: 'top',
+                                      textStyle: {
+                                      color: '#66C5F6',
+                                          fontSize :14
+                                  },
+                                  formatter: '{c}'
+                              }
+                          },
+                          name:'已收货量',
+                              type:'bar',
+                              barWidth: '50%',
+                              itemStyle:{
+                              normal: {
+
+                                  barBorderRadius: 0,
+                                      color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                      offset: 0,
+                                      color: '#96E3D8'
+                                  }, {
+                                      offset: 1,
+                                      color: '#66C5F6'
+                                  }]),
+                                      shadowColor: 'rgba(0, 0, 0, 0)',
+                                      shadowBlur: 20
+                              }
+                          },
+                          data:zhuY2
                   }
+          ],
+          yAxis : [
 
-              ],
-              yAxis : [
-
-                  {   splitLine:{show: false},  //去掉网格
-                      type : 'value',
-                      axisLabel: {
-                          show: true,
-                          textStyle: {
-                              color: '#7d7d7d'
-                          }
-                      },
-                      axisLine:{
-                          lineStyle:{
-                              color:'#FFF',
-                          }
+              {   splitLine:{show: false},  //去掉网格
+                  type : 'value',
+                  axisLabel: {
+                      show: true,
+                      textStyle: {
+                          color: '#7d7d7d'
+                      }
+                  },
+                  axisLine:{
+                      lineStyle:{
+                          color:'#FFF',
                       }
                   }
-              ]
-          };
+              }
+          ]
+      };
           chart3.setOption(option);
       });
   };
+      // 饼图
       createcharts.chart4=function(){
       $(function(){
           var chart4= echarts.init(document.getElementById('chart4'));
@@ -415,6 +412,183 @@
               ]
           };
           chart4.setOption(option);
+      });
+  };
+      // 占比柱图2（出库）
+      createcharts.chart5=function(zhuX,zhuY1,zhuY2,zhuY3){
+      $(function(){
+          var chart5= echarts.init(document.getElementById('chart5'));
+          var option = {
+              //backgroundColor:'rgba(178,220,245,0.4)',//背景色
+              color: ['#3da5a1'],
+              tooltip : {  //鼠标悬浮显示
+                  trigger: 'axis',
+                  axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                      type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                  }
+              },
+              grid: {  //图表的位置
+                  left: '1%',right:'3%',
+                  bottom: '1%',top:'20%',
+                  containLabel: true
+              },
+              legend: {  //图例：
+                  x:'35%',
+                  y:'0%',
+                  data:[
+                      {
+                          name:'出库数量',
+                          icon:'image://./img/2.png'//格式为'image://+icon文件地址'，其中image::后的//不能省略
+                      },
+                      {
+                          name:'库存总量',
+                          icon:'image://./img/1.png'//格式为'image://+icon文件地址'，其中image::后的//不能省略
+                      },
+                      {
+                          name:'预警范围',
+                          icon:'image://./img/3.png'//格式为'image://+icon文件地址'，其中image::后的//不能省略
+                      },
+                  ],
+                  textStyle: {
+                      color: '#7d7d7d;'
+                  },
+              },
+              xAxis : [
+                  {   splitLine:{show: false},  //去掉网格
+                      type : 'category',
+                      data :zhuX,
+                      axisTick: {
+                          alignWithLabel: true
+                      },
+                      axisLabel: {
+                          show: true,
+                          textStyle: {
+                              color: '#7d7d7d'   //x轴字体颜色
+                          },
+                          // interval:0
+                      },
+                      axisLine:{
+                          lineStyle:{
+                              color:'#FFF',   //x轴线颜色
+                          }
+                      },
+
+                  }
+
+              ],
+              series : [
+                  {
+                      // For shadow
+                      label: {
+                          normal: {
+                              show: true,
+                              position: 'top',
+                              textStyle: {
+                                  color: '#96E3D8',
+                                  fontSize :16
+                              },
+                              formatter: '{c}'
+                          }
+                      },
+
+                      type: 'bar',
+                      name:"出库数量",
+                      barWidth: '50%',
+                      itemStyle:{
+                          normal: {
+                              barBorderWidth:1, //边框宽度
+                              barBorderColor: '#96E3D8', //边框色
+                              barBorderRadius: 0,
+                              color: '#fff', //填充色
+                          }
+                      },
+                      barGap:'-100%',
+                      barCategoryGap:'40%',
+                      data:zhuY1,
+                      animation: false
+                  },
+                  {
+                      label: {
+                          normal: {
+                              show: true,
+                              position: 'top',
+                              textStyle: {
+                                  color: '#66C5F6',
+                                  fontSize :14
+                              },
+                              formatter: '{c}'
+                          }
+                      },
+                      name:'库存总量',
+                      type:'bar',
+                      barWidth: '50%',
+                      itemStyle:{
+                          normal: {
+
+                              barBorderRadius: 0,
+                              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                  offset: 0,
+                                  color: '#96E3D8'
+                              }, {
+                                  offset: 1,
+                                  color: '#66C5F6'
+                              }]),
+                              shadowColor: 'rgba(0, 0, 0, 0)',
+                              shadowBlur: 20
+                          }
+                      },
+                      data:zhuY2
+                  },
+                  {
+                      // For shadow
+                      label: {
+                          normal: {
+                              show: true,
+                              position: 'top',
+                              textStyle: {
+                                  color: 'red',
+                                  fontSize :12
+                              },
+                              formatter: '{c}'
+                          }
+                      },
+
+                      type: 'bar',
+                      name:"预警范围",
+                      barWidth: '50%',
+                      itemStyle:{
+                          normal: {
+                              barBorderWidth:2, //边框宽度
+                              barBorderColor: 'red', //边框色
+                              barBorderRadius: 0,
+                              color: '#fff', //填充色
+                          }
+                      },
+                      barGap:'-100%',
+                      barCategoryGap:'40%',
+                      data:zhuY3,
+                      animation: false
+                  },
+              ],
+              yAxis : [
+
+                  {   splitLine:{show: false},  //去掉网格
+                      type : 'value',
+                      axisLabel: {
+                          show: true,
+                          textStyle: {
+                              color: '#7d7d7d'
+                          }
+                      },
+                      axisLine:{
+                          lineStyle:{
+                              color:'#FFF',
+                          }
+                      }
+                  }
+              ]
+          };
+          chart5.setOption(option);
       });
   };
 
--- js/index.js
+++ js/index.js
@@ -14,7 +14,36 @@ $(function(){
             $(".header1 .delete").css("background","#53a2e0");
         }
     );
-// =========入库管理=========================
+//默认日期：
+    //当前日期：
+    function currentTime(){
+        var d=new Date(), str='';
+        str+=d.getFullYear()+'-';
+        str+=("0" + (d.getMonth() + 1)).slice(-2)+'-';
+        str+=("0" + d.getDate()).slice(-2);
+        return str;
+    }
+    //前一个月
+    function getlastmonth() {
+        var currentYear=new Date().getFullYear();
+        var currentMonth=new Date().getMonth()+1;
+        var lastMonth=new Date().getMonth();
+        var currentDate=("0"+new Date().getDate()).slice(-2);
+        var prevCurrentYear=0;
+        var prevCurrentMonth=0;
+        if(currentMonth==1){
+            prevCurrentYear=currentYear-1;
+            prevCurrentMonth=12;
+        }else{
+            prevCurrentYear=currentYear;
+            prevCurrentMonth=("0" + (currentMonth-1)).slice(-2);
+        }
+        var lastmonth=prevCurrentYear+"-"+prevCurrentMonth+"-"+currentDate;
+        return lastmonth;
+    }
 
-
+    var from=getlastmonth();
+    var to=currentTime();
+    $(".outer #from input").attr("value",from);
+    $(".outer #to input").attr("value",to);
 });
