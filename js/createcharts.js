c88dca56fc25961940f7877a172d4dead0f315b2
XIAOkeji6 <you@example.com>
2018-01-15 15:42:03 +0800

    aa

--- js/createcharts.js
+++ js/createcharts.js
@@ -229,11 +229,252 @@
                   }
 
               ]
+
           };
           chart2.setOption(option);
       });
 
+
+
   };
+          // 横线柱图(带跳转【入库】)
+          createcharts.chart6=function(title,Xdata,Ydata,Ydata2){
+          $(function(){
+              var chart6= echarts.init(document.getElementById('chart6'));
+              var option = {
+                  //backgroundColor:'rgba(178,220,245,0.4)',//背景色
+                  color: ['#3da5a1'],
+                  title: {
+                      text:title,
+                      textStyle: {
+                          fontSize:22,
+                          fontWeight:'normal',
+                          color:'#218be2',            //标题颜色
+                      },
+                      x:'center',
+                      y:'14px'
+
+                  },
+                  tooltip : {  //鼠标悬浮显示
+                      trigger: 'axis',
+                      axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                          type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                      }
+                  },
+                  grid: {  //图表的位置
+                      left: '2%',
+                      right: '1%',
+                      bottom: '2%',
+                      containLabel: true
+                  },
+                  xAxis : [
+                      {   splitLine:{show: false},  //去掉网格
+                          type : 'value',
+                          axisLabel: {
+                              show: true,
+                              textStyle: {
+                                  color: '#fff'   //x轴字体颜色
+                              },
+                              // interval:0
+                          },
+                          axisLine:{
+                              lineStyle:{
+                                  color:'#fff',   //x轴线颜色
+                              }
+                          }
+                      }
+                  ],
+                  series : [
+                      {
+                          label: {
+                              normal: {
+                                  show: true,
+                                  position: 'inside',
+                                  formatter: '{c}%'
+                              }
+                          },
+                          name:title,
+                          type:'bar',
+                          barWidth: '70%',
+                          itemStyle:{
+                              normal: {
+
+                                  barBorderRadius:25,
+                                  color: new echarts.graphic.LinearGradient(0, 0, 1, 0,
+                                      [
+                                          {offset: 0, color: '#bd8cf5'},
+                                          {offset: 1, color: '#6be2e1'}
+                                      ]
+                                  ),
+                                  shadowColor: 'rgba(0, 0, 0, 0.4)',
+                                  shadowBlur: 20
+                              }
+                          },
+                          data:Ydata
+                      }
+                  ],
+                  yAxis : [
+                      {
+                          type : 'category',
+                          data :Xdata,
+                          axisTick: {
+                              alignWithLabel: true
+                          },
+                          axisLabel: {
+                              show: true,
+                              textStyle: {
+                                  color: '#7d7d7d'   //x轴字体颜色
+                              },
+                              // interval:0
+                          },
+                          axisLine:{
+                              lineStyle:{
+                                  color:'#fff',   //x轴线颜色
+                              }
+                          }
+
+                      }
+
+                  ]
+
+              };
+              chart6.setOption(option);
+              //每条柱状的点击事件：
+              chart6.on('click', function (params) {
+                  //var index= params.dataIndex;   //柱图下标
+                  var name = params.name;
+                  if(name=="所有项目"){
+                      window.location.href = "In2_xianqing.html";
+                  }else{
+                      window.location.href = "In2_xianqing_xq.html";
+                  }
+
+              })
+          });
+
+
+
+      };
+          // 横线柱图(带跳转【出库】)
+          createcharts.chart7=function(title,Xdata,Ydata,Ydata2){
+                  $(function(){
+                      var chart7= echarts.init(document.getElementById('chart7'));
+                      var option = {
+                          //backgroundColor:'rgba(178,220,245,0.4)',//背景色
+                          color: ['#3da5a1'],
+                          title: {
+                              text:title,
+                              textStyle: {
+                                  fontSize:22,
+                                  fontWeight:'normal',
+                                  color:'#218be2',            //标题颜色
+                              },
+                              x:'center',
+                              y:'14px'
+
+                          },
+                          tooltip : {  //鼠标悬浮显示
+                              trigger: 'axis',
+                              axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                                  type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                              }
+                          },
+                          grid: {  //图表的位置
+                              left: '2%',
+                              right: '1%',
+                              bottom: '2%',
+                              containLabel: true
+                          },
+                          xAxis : [
+                              {   splitLine:{show: false},  //去掉网格
+                                  type : 'value',
+                                  axisLabel: {
+                                      show: true,
+                                      textStyle: {
+                                          color: '#fff'   //x轴字体颜色
+                                      },
+                                      // interval:0
+                                  },
+                                  axisLine:{
+                                      lineStyle:{
+                                          color:'#fff',   //x轴线颜色
+                                      }
+                                  }
+                              }
+                          ],
+                          series : [
+                              {
+                                  label: {
+                                      normal: {
+                                          show: true,
+                                          position: 'inside',
+                                          formatter: '{c}%'
+                                      }
+                                  },
+                                  name:title,
+                                  type:'bar',
+                                  barWidth: '70%',
+                                  itemStyle:{
+                                      normal: {
+
+                                          barBorderRadius:25,
+                                          color: new echarts.graphic.LinearGradient(0, 0, 1, 0,
+                                              [
+                                                  {offset: 0, color: '#bd8cf5'},
+                                                  {offset: 1, color: '#6be2e1'}
+                                              ]
+                                          ),
+                                          shadowColor: 'rgba(0, 0, 0, 0.4)',
+                                          shadowBlur: 20
+                                      }
+                                  },
+                                  data:Ydata
+                              }
+                          ],
+                          yAxis : [
+                              {
+                                  type : 'category',
+                                  data :Xdata,
+                                  axisTick: {
+                                      alignWithLabel: true
+                                  },
+                                  axisLabel: {
+                                      show: true,
+                                      textStyle: {
+                                          color: '#7d7d7d'   //x轴字体颜色
+                                      },
+                                      // interval:0
+                                  },
+                                  axisLine:{
+                                      lineStyle:{
+                                          color:'#fff',   //x轴线颜色
+                                      }
+                                  }
+
+                              }
+
+                          ]
+
+                  };
+                  chart7.setOption(option);
+                  //每条柱状的点击事件：
+                  chart7.on('click', function (params) {
+                      //var index= params.dataIndex;   //柱图下标
+                      var name = params.name;
+                      if(name=="所有项目"){
+                          window.location.href = "Out2_xianqing.html";
+                      }else{
+                          window.location.href = "Out2_xianqing_xq.html";
+                      }
+
+                  })
+              });
+
+
+
+            };
+
+
       // 占比柱图1（入库）
       createcharts.chart3=function(zhuX,zhuY1,zhuY2){
       $(function(){
