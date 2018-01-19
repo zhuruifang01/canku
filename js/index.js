922d41a94863b134419cbe36cdc271df781e80f0
XIAOkeji6 <you@example.com>
2018-01-19 14:10:43 +0800

    vv

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 $(function(){
-//left_menu
-    $(".sideleft ul").click(function () {
-        var index=$(this).index(".sideleft ul");
-        $(".sideleft ul").removeClass("active");
-        $(".sideleft ul").eq(index).addClass("active");
-        $("iframe").css("display","none").eq(index).css("display","block");
-    });
-// ========人员管理==========================
+ // ========人员管理==========================
 //number.html
     $(".header1 .xuanze").click(function(){
             $(".contain1 .conn .con .checkbox").css("display","block");
